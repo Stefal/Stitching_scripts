9305c735e47b12176c1f7ede2d569001f6b5b1fc
Stefal <stephane.peneau@wanadoo.fr>
2021-12-13 16:20:17 +0100

    initial commit

---
+++ launch_stitching.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+i=1
+for dir in */ ; do
+    #cp autostitch.sh "${dir}"/autostitch
+    cd "${dir}"
+    #./autostitch
+    #mv default4.jpg "${d%/}".jpg
+    # "${d%/}" is the directory name with the trailing "/"" removed
+    cd ..
+    i=$((i + 1))
+done
+echo $i 'folders managed'
+echo $(find . -maxdepth 2 -type f -name "*.jpg" -newermt '1 day ago' | wc -l) 'pano created'

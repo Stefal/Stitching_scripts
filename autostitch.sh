3dc14791db494d310f968d952c078c0ef6a72fe5
Stefal <stephane.peneau@wanadoo.fr>
2021-12-14 17:20:05 +0100

    trying to reduce vignetting

--- autostitch.sh
+++ autostitch.sh
@@ -13,12 +13,14 @@ exiftool='/mnt/c/Photos_OSM/exiftool/exiftool.exe'
 "${pto_gen}" --projection=3 --fov=125 --stacklength=1 --output=autobase.pto APN0.jpg APN1.jpg APN2.jpg APN3.jpg APN4.jpg APN5.jpg
 "${pto_var}" --set=p0=-12,y0=0,p1=-12,y1=90,p2=-12,y2=180,p3=-12,y3=-90,p4=45,y4=90,p5=45,y5=-90,a=0.0248756,b=-0.033,c=0.0524907 --opt=Eev,r0,p0,v0,v --modify-opt --unlink=v0,Ra0,Rb0,Rc0,Rd0,Re0,a0,b0,c0,d0,g0,t0,Va0,Vb0,Vc0,Vd0,Vx0,Vy0 -o autobase2.pto autobase.pto
 "${pto_var}" --opt=Er1,Eb1,Er2,Eb2,Er3,Eb3,Er4,Eb4,Er5,Eb5,r0,p0,v0,v --modify-opt -o autobase3.pto autobase2.pto
-"${pano_modify}" --output-exposure=AUTO --output-range-compression=2 --ldr-file=JPG --ldr-compression=95 --canvas=13340x6670 -o autobase4.pto autobase3.pto
-"${cpfind}" --prealigned --sieve1width 25 --sieve1height 25 --sieve1size 625 --kdtreesteps 300 -o default1.pto autobase4.pto
+"${pano_modify}" --output-exposure=AUTO --output-range-compression=1 --ldr-file=JPG --ldr-compression=95 --canvas=13340x6670 -o autobase4.pto autobase3.pto
+"${cpfind}" --prealigned --celeste --sieve1width 20 --sieve1height 20 --sieve1size 200 --kdtreesteps 300 -o default1.pto autobase4.pto
 "${linefind}" --image=0 --image=1 --image=2 --image=3 -o default2.pto default1.pto
 "${cpclean}" -o default3.pto default2.pto
-"${autooptimiser}" -m -l -n -o default4.pto default3.pto
-"${pano_modify}" --straighten -o default5.pto default4.pto
+sed -i '/#hugin_optimizerPhotoMasterSwitch/c\#hugin_optimizerPhotoMasterSwitch 23' default3.pto
+"${autooptimiser}" -l -n -o default4.pto default3.pto
+"${autooptimiser}" -m -o default5.pto default4.pto
+"${pano_modify}" --straighten -o default5.pto default5.pto
 sed -i '/#hugin_blender enblend/c\#hugin_blender internal' default5.pto
 sed -i '/#hugin_verdandiOptions/c\#hugin_verdandiOptions --seam=blend' default5.pto
 "${hugin_executor}" default5.pto --stitching

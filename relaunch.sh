3fe69a4ba058f71e5f7751c9cb0e4074e07b80fc
Stefal <stephane.peneau@wanadoo.fr>
2022-05-04 11:33:48 +0200

    add wsl/linux path check

--- relaunch.sh
+++ relaunch.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
-hugin_executor='/mnt/c/Program Files/Hugin/bin/hugin_executor.exe'
-exiftool='/mnt/c/Photos_OSM/exiftool/exiftool.exe'
+if [ $(uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip') ];
+then
+    pto_gen='/mnt/c/Program Files/Hugin/bin/pto_gen.exe'
+    pto_var='/mnt/c/Program Files/Hugin/bin/pto_var.exe'
+    pano_modify='/mnt/c/Program Files/Hugin/bin/pano_modify.exe'
+    cpfind='/mnt/c/Program Files/Hugin/bin/cpfind.exe'
+    linefind='/mnt/c/Program Files/Hugin/bin/linefind.exe'
+    cpclean='/mnt/c/Program Files/Hugin/bin/cpclean.exe'
+    autooptimiser='/mnt/c/Program Files/Hugin/bin/autooptimiser.exe'
+    hugin_executor='/mnt/c/Program Files/Hugin/bin/hugin_executor.exe'
+    exiftool='/mnt/c/Photos_OSM/exiftool/exiftool.exe'
+    pto_lensstack='/mnt/c/Program Files/Hugin/bin/pto_lensstack.exe'
+    vig_optimize='/mnt/c/Program Files/Hugin/bin/vig_optimize.exe'
+    checkpto='/mnt/c/Program Files/Hugin/bin/checkpto.exe'
+else
+    pto_gen=$(which pto_gen)
+    pto_var=$(which pto_var)
+    pano_modify=$(which pano_modify)
+    cpfind=$(which cpfind)
+    linefind=$(which linefind)
+    cpclean=$(which cpclean)
+    autooptimiser=$(which autooptimiser)
+    hugin_executor=$(which hugin_executor)
+    exiftool=$(which exiftool)
+    pto_lensstack=$(which pto_lensstack)
+    vig_optimize=$(which vig_optimize)
+    checkpto=$(which checkpto)
+fi
+
 i=1
 
 for dir in $(find . -maxdepth 2 -type f -name "final.pto" -newermt '2 hour ago') ; do

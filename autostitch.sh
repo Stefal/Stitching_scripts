6c706427e62a40aa3bc71268d6b0a1e9ce45605f
Stefal <stephane.peneau@wanadoo.fr>
2022-05-04 11:38:10 +0200

    Update autostitch.sh
    More vig_optimize
    I hope it's better

--- autostitch.sh
+++ autostitch.sh
@@ -15,7 +15,6 @@ then
     pto_lensstack='/mnt/c/Program Files/Hugin/bin/pto_lensstack.exe'
     vig_optimize='/mnt/c/Program Files/Hugin/bin/vig_optimize.exe'
     checkpto='/mnt/c/Program Files/Hugin/bin/checkpto.exe'
-
 else
     pto_gen=$(which pto_gen)
     pto_var=$(which pto_var)
@@ -29,14 +28,12 @@ else
     pto_lensstack=$(which pto_lensstack)
     vig_optimize=$(which vig_optimize)
     checkpto=$(which checkpto)
-
 fi
 
 #use these Y/P/R variables to rotate the pano
 Yaw=0
 Pitch=0
 Roll=0
-
 Vb_Count=0
 Ev_Count=0
 Vb_max=5
@@ -66,17 +63,18 @@ test -f apn0.jpg || exit 1
 "${pto_lensstack}" --new-lens i1,i2,i3,i4,i5 -o autobase3.pto autobase2.pto
 "${pto_var}" --opt=r0,p0,v0,v,!Ra,!Rb,!Rc,!Rd,!Rd,!Re,Eev,Er,Eb,Vc,Vd,!Er0,!Eb0 --modify-opt -o autobase4.pto autobase3.pto
 "${vig_optimize}" -o autobase5.pto autobase4.pto
-"${cpfind}" --prealigned --celeste --sieve1width 20 --sieve1height 20 --sieve1size 200 --kdtreesteps 300 -o default1.pto autobase5.pto
+"${cpfind}" --prealigned --sieve1width 20 --sieve1height 20 --sieve1size 200 --kdtreesteps 300 -o default1.pto autobase5.pto
 #"${linefind}" --image=0 --image=1 --image=2 --image=3 -o default2.pto default1.pto
 "${cpclean}" -o default2.pto default1.pto
-"${checkpto}" default2.pto | grep -q 'unconnected images' && "${cpfind}" --fullscale --multirow --sieve1width=25 --sieve1height=25 --sieve1size=1000 --kdtreesteps=300 -o default1.pto default1.pto
+"${checkpto}" default2.pto | grep -q 'unconnected images' && "${cpfind}" --fullscale --multirow --sieve1width=25 --sieve1height=25 --sieve1size=1000 --kdtreesteps=300 -o default2.pto default2.pto
 "${autooptimiser}" -l -n -o default3.pto default2.pto
 #"${pano_modify}" --straighten -o default5.pto default4.pto
 "${pano_modify}" --rotate=$Yaw,$Pitch,$Roll -o default5.pto default3.pto
 sed -i '/#hugin_blender enblend/c\#hugin_blender internal' default5.pto
 sed -i '/#hugin_verdandiOptions/c\#hugin_verdandiOptions --seam=blend' default5.pto
+"${pano_modify}" --output-exposure=AUTO --output-range-compression=1 -o default5.pto default5.pto
+"${vig_optimize}" -o default5.pto default5.pto
 "${pano_modify}" --output-exposure=AUTO --output-range-compression=1 --ldr-file=JPG --ldr-compression=90 --canvas=13340x6670 -o final.pto default5.pto
-
 #Try to fix lens exposure when Vb is too high
 while ! check_Vb final.pto $Vb_max $((-Vb_max))
 do
@@ -102,7 +100,6 @@ do
         ((Ev_Count=Ev_Count+1))
     fi
 done
-
 check_Fov final.pto $Fov_max $Fov_min || touch check_fov
 "${hugin_executor}" final.pto --stitching
 "${exiftool}" -TagsFromFile APN0.jpg -DateTimeOriginal -SubSecTimeOriginal -Make=STFMANI -Model=V6MPack final.jpg -overwrite_original

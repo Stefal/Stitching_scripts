0c46c41432d1538adbca77e3ec15d3aa7db64cfb
Stefal <stephane.peneau@wanadoo.fr>
2022-05-20 15:01:01 +0200

    Update stitch_missing_pano.sh

--- stitch_missing_pano.sh
+++ stitch_missing_pano.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 S_PATH="$(realpath "$(dirname $0)")"
-
+i=0
  for dir in */ ; do
     if ! $(ls "${dir}"/"${dir%/}".jpg > /dev/null 2>&1)
     then
-        echo 'missing pano in ' "${dir}"
-        cp "${S_PATH}"/autostitch.sh "${dir}"/autostitch.sh
-        cd "${dir}"
-        ./autostitch.sh
-        mv final.jpg "${dir%/}".jpg
-        #"${d%/}" is the directory name with the trailing "/"" removed
-        cd ..
+        echo 'missing pano in ' "${dir}""${dir%/}"'.jpg'
+        i=$((i + 1)) 
+        #echo '$1: ' $1
+        if [ "$1" = '--stitch' ]
+            then
+            echo 'dans le if'
+            cp "${S_PATH}"/autostitch.sh "${dir}"/autostitch.sh
+            cd "${dir}"
+            ./autostitch.sh
+	        mv final.jpg "${dir%/}".jpg
+            cd ..
+        fi
+        ##"${d%/}" is the directory name with the trailing "/"" removed
     fi
 done
+echo 'Missing panos: ' $i

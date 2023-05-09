097f05fe8dd2f37f6a4282d1cfab1471144453ab
Stefal <stephane.peneau@wanadoo.fr>
2023-05-09 21:54:18 +0200

    Update launch_stitching.sh
    Launch from any directory

--- launch_stitching.sh
+++ launch_stitching.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+STITCHING_SCRIPT="${1}" || 'autostitch.sh'
 S_PATH="$(realpath "$(dirname $0)")"
 i=1
 for dir in */ ; do
-    cp "${S_PATH}"/autostitch.sh "${dir}"/autostitch.sh
+    cp "${S_PATH}"/"${STITCHING_SCRIPT}" "${dir}"/"${STITCHING_SCRIPT}"
     cd "${dir}"
-    ./autostitch.sh
+    ./"${STITCHING_SCRIPT}"
     mv final.jpg "${dir%/}".jpg
     #"${d%/}" is the directory name with the trailing "/"" removed
     cd ..
